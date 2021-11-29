6adb3d31187a5146b0ea30414b0aeaaa4e57f02b
DavidBlomeyer <89857190+DavidBlomeyer@users.noreply.github.com>
2021-11-28 22:14:51 -0500

    Add project files.

---
+++ Fledgling.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fledgling.WebMVC.MvcApplication" Language="C#" %>
---
+++ Fledgling.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fledgling.WebMVC.Startup))]
+namespace Fledgling.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
